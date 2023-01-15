6e89d797382327a4133331988d06a8263eb45b5e
unknown <zavaremre@gmail.com>
2023-01-15 11:50:00 +0300

    added content

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
       id: 1,
       title: "Jotform",
       position: "UI Developer",
-      type: "Fulltime",
-      time: "Now",
+      type: "Tam Zamanlı",
+      time: "Şuan",
       left: false,
       isActive: true,
     },
@@ -23,8 +23,8 @@ function App() {
       id: 2,
       title: "Allesgo",
       position: "Front End Developer",
-      type: "Fulltime",
-      time: "+4 Year",
+      type: "Tam Zamanlı",
+      time: "+4 Yıl",
       left: true,
     },
     {
@@ -32,7 +32,7 @@ function App() {
       title: "Bionluk",
       position: "Wordpress Developer",
       type: "Freelance",
-      time: "+1 Year",
+      time: "+1 Yıl",
       left: false,
     },
     {
@@ -40,15 +40,15 @@ function App() {
       title: "Platform New Media",
       position: "UI Wordpress Developer",
       type: "Remote",
-      time: "+1 Year",
+      time: "+1 Yıl",
       left: true,
     },
     {
       id: 5,
       title: "Medanis",
       position: "UI Wordpress Developer",
-      type: "Fulltime",
-      time: "+2 Year",
+      type: "Tam Zamanlı",
+      time: "+2 Yıl",
       left: false,
     },
   ]);
@@ -56,24 +56,21 @@ function App() {
     {
       id: 1,
       title: "",
-      content:
-        "Derd-i firakın ile düşeli sevdaya mey'e Müptelayım, deliyim, düşmüşüm esrarı-ney'e Feleğin kahpe başında paralansın parası Ben güzel sevmeye geldim, değil ekmek yemeye",
+      content: "Derd-i firakın ile düşeli sevdaya mey'e Müptelayım, deliyim, düşmüşüm esrarı-ney'e Feleğin kahpe başında paralansın parası Ben güzel sevmeye geldim, değil ekmek yemeye",
       author: "Neyzen Tevfik",
       image: "neyzen.png",
     },
     {
       id: 2,
       title: "",
-      content:
-        "Kaç yol, ağlamaklı olmuşum geceleri, Asıl, bizim aramızda güzeldir hasret Ve asıl biz biliriz kederi.",
+      content: "Kaç yol, ağlamaklı olmuşum geceleri, Asıl, bizim aramızda güzeldir hasret Ve asıl biz biliriz kederi.",
       author: "Ahmed Arif",
       image: "ahmed-arif.png",
     },
     {
       id: 3,
       title: "",
-      content:
-        "Kör karanlıkta açardık paslı gözlerimizi Dilimizde akşamdan kalma bir küfür Salonlar piyasalar sanat-sevicileri Derdim günüm insan arasına çıkarmaktı seni Yakanda bir amonyak çiçeği",
+      content: "Kör karanlıkta açardık paslı gözlerimizi Dilimizde akşamdan kalma bir küfür Salonlar piyasalar sanat-sevicileri Derdim günüm insan arasına çıkarmaktı seni Yakanda bir amonyak çiçeği",
       author: "Can Yücel",
       image: "can-yucel.png",
     },
@@ -91,18 +88,13 @@ function App() {
   };
 
   return (
-    <main className="mt-20">
+    <main className='mt-20'>
       <Darkmode />
       <section className={classNames("container mb-10", { pill: true })}>
-        <div className="relative flex justify-center w-full mb-3">
+        <div className='relative flex justify-center w-full mb-3'>
           <Avatar data={data} />
-          <div className="pointer-events-none select-none hidden sm:block absolute ml-44 -mt-14 rotate-45">
-            <object
-              width="250px"
-              height="250px"
-              type="image/svg+xml"
-              data={circle}
-            >
+          <div className='pointer-events-none select-none hidden sm:block absolute ml-44 -mt-14 rotate-45'>
+            <object width='250px' height='250px' type='image/svg+xml' data={circle}>
               Masthead animation
             </object>
           </div>
@@ -111,31 +103,26 @@ function App() {
         <Socialmedia />
         <div>
           <Bio />
-          <div className="socials mb-4"></div>
-          <div className="flex justify-center">
-            <button
-              onClick={scrollTo}
-              className="text-blue-600 text-xs text-center font-semibold"
-            >
-              Read More
+          <div className='socials mb-4'></div>
+          <div className='flex justify-center'>
+            <button onClick={scrollTo} className='text-blue-600 text-xs text-center font-semibold'>
+              Antidepresan
             </button>
           </div>
         </div>
       </section>
 
-      <section className="container mb-10">
+      <section className='container mb-10'>
         <div className='relative after:content-[""]  after:absolute  after:top-0  after:left-1/2  after:right-0  after:w-px  after:h-full  after:bg-gray-100 after:dark:bg-slate-800 after:-ml-[0.5px]'>
           {timeline.map((t, index) => {
             return <Timeline key={index} timeline={t} />;
           })}
         </div>
       </section>
-      <section className="container mb-10">
-        <div className="w-full h-px bg-gray-100 dark:bg-slate-800 mb-5"></div>
-        <div className="inline-flex justify-center items-center w-full mb-5">
-          <h2 className="text-sm font-medium border-b-2 border-gray-600 dark:border-slate-800 pb-0.5">
-            Blog
-          </h2>
+      <section className='container mb-10'>
+        <div className='w-full h-px bg-gray-100 dark:bg-slate-800 mb-5'></div>
+        <div className='inline-flex justify-center items-center w-full mb-5'>
+          <h2 className='text-sm font-medium border-b-2 border-gray-600 dark:border-slate-800 pb-0.5'>Şiir & Çay</h2>
         </div>
 
         <section>
--- src/Components/Bio.js
+++ src/Components/Bio.js
@@ -2,12 +2,9 @@ import React from "react";
 
 function Bio() {
   return (
-    <p className="text-center text-xs leading-5 mb-4">
-      “Donec dignissim est ac odio condimentum, sit amet vulputate dui suscipit.
-      Etiam non magna dignissim, fermentum enim vel, bibendum nunc. Class aptent
-      taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-      himenaeos. Proin feugiat quam eget nibh suscipit aliquam. Suspendisse
-      potenti. Nam efficitur lobortis eli.”
+    <p className='text-center text-xs leading-5 mb-4'>
+      “2000'li yılların başında internet kafeler de başlayan yazılım merakım buralara da kadar geldi. 10 yıl aşkın süredir çeşitli firmalarda tam zamanlı remote, freelance olarak görev yaptım. Şuan
+      Jotform firmasında UI DEVELOPER olarak görev yapıyorum. Kısacası bilgisayar başında geçen heyecanlı bi macera”
     </p>
   );
 }
