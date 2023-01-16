e63b771750a9a968b6382e66ea9a792aafb9b1c6
unknown <zavaremre@gmail.com>
2023-01-16 22:32:03 +0300

    timeline dots

--- src/App.js
+++ src/App.js
@@ -137,8 +137,8 @@ function App() {
           <div className='socials mb-4'></div>
           <div className='flex justify-center'>
             <button onClick={scrollTo} className='text-blue-600 text-xs text-center font-semibold animate-bounce'>
-              <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' class='bi bi-chevron-down' viewBox='0 0 16 16'>
-                <path fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z' />
+              <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' fill='currentColor' className='bi bi-chevron-down' viewBox='0 0 16 16'>
+                <path fillRule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z' />
               </svg>
             </button>
           </div>
